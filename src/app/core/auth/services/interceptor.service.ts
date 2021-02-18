cd0ed0fbeba1bb00912a72f8a5e499ae376040f8
isidro.Medina <Isidro.Medina@ust-global.com>
2021-02-18 12:36:09 +0100

    Update Login

--- src/app/core/auth/services/interceptor.service.ts
+++ src/app/core/auth/services/interceptor.service.ts
@@ -1,38 +1,43 @@
-import { HttpErrorResponse, HttpEvent, HttpHandler, HttpHeaders, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import {
+  HttpErrorResponse,
+  HttpEvent,
+  HttpHandler,
+  HttpHeaders,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { AuthService } from 'src/app/features/auth/services/auth.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class InterceptorService implements HttpInterceptor {
+  constructor(private _auth: AuthService) {}
 
-  constructor(private _auth: AuthService) { }
-
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
     const headers = new HttpHeaders({
-      Authorization: 'Bearer ' + this._auth.getToken()
+      Authorization: 'Bearer ' + this._auth.getToken(),
+      //Habrá que mandar tambien el idioma desde el translate service
     });
 
     const reqClone = req.clone({
-      headers
+      headers,
     });
 
-    return next.handle( reqClone ).pipe(
-      catchError( this.manejarError )
-    );
+    return next.handle(reqClone).pipe(catchError(this.manejarError));
   }
 
-  manejarError( error: HttpErrorResponse ) {
+  manejarError(error: HttpErrorResponse) {
     console.log('Sucedió un error');
     console.log('Registrado en el log file');
     console.log(error.error.mensaje);
     console.warn(error);
     return throwError('Error personalizado');
   }
-
-
 }
