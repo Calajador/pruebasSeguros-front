8f159196f201d429fcbc07e8ba2853be3e2d92c7
Jimmy Torres <jimmy.torres@ust-global.com>
2021-04-19 18:27:43 -0500

    Add: Se agrega contenido en la vista de busqueda agentes, se modifica estilos en los botones

--- src/app/features/agentes/buscar/buscar.component.ts
+++ src/app/features/agentes/buscar/buscar.component.ts
@@ -1,15 +1,64 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  ColorButtonEnum,
+  TypeButtonEnum,
+} from 'src/app/shared/components/button/button.component';
 
 @Component({
   selector: 'app-buscar',
   templateUrl: './buscar.component.html',
-  styleUrls: ['./buscar.component.css']
+  styleUrls: ['./buscar.component.css'],
 })
 export class BuscarComponent implements OnInit {
+  miFormulario: FormGroup = this.fb.group({
+    codigo: ['', Validators.required],
+    matricula: ['', Validators.required],
+    cuit: ['', Validators.required],
+    grupoEconomico: ['', Validators.required],
+    tipoPersona: ['', Validators.required],
+    tipoDocumento: ['', Validators.required],
+    numeroDocumento: ['', Validators.required],
+    nombre: ['', Validators.required],
+    figuraRed: ['', Validators.required],
+    cat: ['', Validators.required],
+    provincia: ['', Validators.required],
+    localidad: ['', Validators.required],
+    figuraRed2: ['', Validators.required],
+    codigoFigura: ['', Validators.required],
+  });
 
-  constructor() { }
+  itemSelected: any;
 
-  ngOnInit(): void {
+  public readonly ButtonTypes = TypeButtonEnum;
+  public readonly ButtonColors = ColorButtonEnum;
+  grupoEconomicoOpt: string[] = ['opt1', 'opt2', 'opt3', 'opt4'];
+
+  columnHeader = {
+    agente: 'Agente',
+    codigo: 'Código',
+  };
+  dataTable = [
+    { agente: 'Rozo Serrano Jose Miguel', codigo: '234562' },
+    { agente: 'Delima Demarch', codigo: '123678' },
+    { agente: 'Castaño Russo Ana Milena', codigo: '876903' },
+  ];
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {}
+
+  guardar() {
+    if (this.miFormulario.invalid) {
+      this.miFormulario.markAllAsTouched();
+    }
   }
 
+  select(item: any) {
+    this.itemSelected = item[0];
+  }
+
+  get btnDisabled() {
+    return this.itemSelected === undefined || this.itemSelected === null;
+  }
 }
