e2a6f1551a4f1a857b9268a9d374d4e82ff13ebc
Jimmy Torres <jimmy.torres@ust-global.com>
2021-04-22 18:07:36 -0500

    Add: Se agrega la vista cobranza

--- src/app/features/comercial/intermediarios/intermediarios.module.ts
+++ src/app/features/comercial/intermediarios/intermediarios.module.ts
@@ -5,9 +5,10 @@ import { IntermediariosRoutingModule } from './intermediarios-routing.module';
 import { GestionComponent } from './gestion/gestion.component';
 import { DatosFiguraComponent } from './gestion/datos-figura/datos-figura.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { CobranzasComponent } from './gestion/cobranzas/cobranzas.component';
 
 @NgModule({
-  declarations: [GestionComponent, DatosFiguraComponent],
+  declarations: [GestionComponent, DatosFiguraComponent, CobranzasComponent],
   imports: [CommonModule, IntermediariosRoutingModule, SharedModule],
 })
 export class IntermediariosModule {}
